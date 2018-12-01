1505792b2c0f8f0b8a0b89433230d5a6bd7715d7
Harrison McCullough <mccullough.harrison@gmail.com>
2018-12-01 15:46:44 -0700

    Finish day01a

---
+++ 2018/day01a.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import sys
+
+def solve(frequencies):
+    return sum(frequencies)
+
+def get_input():
+    return [int(w) for w in sys.stdin.readlines()]
+
+def main():
+    frequencies = get_input()
+    print(frequencies)
+    print(solve(frequencies))
+
+if __name__ == '__main__':
+    main()
