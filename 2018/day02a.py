c32bcca6c0add2c0b15ef5b421a2b114c54ac541
Harrison McCullough <mccullough.harrison@gmail.com>
2018-12-02 16:12:28 -0700

    Start day02

---
+++ 2018/day02a.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import sys
+
+def solve(box_ids):
+    pass
+
+def get_input():
+    return sys.stdin.readlines()
+
+def main():
+    box_ids = get_input()
+    print(solve(box_ids))
+
+if __name__ == '__main__':
+    main()
