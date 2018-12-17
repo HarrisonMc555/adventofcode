7957415901215881c08f82e13f97bca719fd7d73
Harrison McCullough <mccullough.harrison@gmail.com>
2018-12-16 21:29:55 -0700

    Implement move (untested)

--- 2018/day15a.py
+++ 2018/day15a.py
@@ -8,6 +8,12 @@ from enum import Enum, auto
 # Classes
 ################################################################################
 
+class NoMoreTargetsException(Exception):
+    pass
+
+class NoReachablePositions(Exception):
+    pass
+
 class Team(Enum):
     ELF = auto()
     GOBLIN = auto()
@@ -20,6 +26,9 @@ class Tile:
     def is_occupied(self):
         return self.is_wall or self.occupant is not None
 
+    def is_unoccupied(self):
+        return not self.is_occupied()
+
     def has_unit(self):
         return self.occupant is not None
 
@@ -27,6 +36,16 @@ class Tile:
         if self.occupant is None:
             raise Exception('No unit at tile')
         return self.occupant
+
+    def remove_occupant(self):
+        if self.occupant is None:
+            raise Exception('Remove occupant from empty tile')
+        self.occupant = None
+
+    def add_occupant(self, occupant):
+        if self.occupant is not None:
+            raise Exception('Add occupant to occupied tile')
+        self.occupant = occupant
 
 class Unit:
     STARTING_HP = 200
@@ -42,13 +61,89 @@ class Unit:
         return self.row, self.col
 
     def tick(self, units):
-        pass
+        did_move = self.move(units)
+        killed_unit = self.attack(units)
+        return did_move, killed_unit
+
+    def move(self, units):
+        targets = self.get_targets(units)
+        if not targets:
+            raise NoMoreTargetsException()
+        target_adjacent_positions = get_unit_valid_adjacent_positions(targets)
+        if self.get_position() in target_adjacent_positions:
+            return False
+        destination = self.select_destination(target_adjacent_positions)
+        step_position = self.select_first_step_towards(destination)
+        if step_position and step_position != self.get_position():
+            self.move_to(step_position)
+            return True
+        return False
 
     def get_targets(self, units):
         return {unit for unit in units if unit.team != self.team}
 
-    def select_target(self, targets):
-        pass
+    def select_destination(self, destinations):
+        closest_positions = select_closest_positions(self.get_position(),
+                                                     destinations, self.grid)
+        assert closest_positions
+        # choose based on reading order
+        return sorted(closest_positions)[0]
+
+    def select_first_step_towards(self, position):
+        closest_to_me = select_closest_positions(position, self.get_position(),
+                                                 self.grid)
+        assert closest_to_me
+        # choose based on reading order
+        return sorted(closest_to_me)[0]
+
+    def move_to(self, destination):
+        self.grid[self.row][self.col].remove_occupant()
+        row, col = destination
+        self.row = row
+        self.col = col
+        self.grid[self.row][self.col].add_occupant(self)
+
+    def get_valid_adjacent_positions(self):
+        return get_valid_adjacent_positions(self.get_position(), self.grid)
+
+    def attack(self, units):
+        if units:
+            raise Exception('Unimplemented')
+        return self
+
+def get_unit_valid_adjacent_positions(units):
+    positions = set()
+    for unit in units:
+        positions.update(unit.get_valid_adjacent_positions())
+    return positions
+
+DIDJS = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+def get_adjacent_positions(position):
+    i, j = position
+    return [(i + di, j + dj) for di, dj in DIDJS]
+
+def get_valid_adjacent_positions(position, grid):
+    return [(i, j) for i, j in get_adjacent_positions(position)
+            if grid[i][j].is_unoccupied()]
+
+def select_closest_positions(start, destinations, grid):
+    cur_positions = {start}
+    visited = set()
+    closest_positions = set()
+    while not closest_positions:
+        next_positions = set()
+        if not cur_positions:
+            raise NoReachablePositions()
+        for position in cur_positions:
+            if position in destinations:
+                closest_positions.add(position)
+            else:
+                next_positions.update(get_valid_adjacent_positions(position,
+                                                                   grid))
+            visited.add(position)
+        next_positions.difference_update(visited)
+        cur_positions = next_positions
+    return closest_positions
 
 ################################################################################
 # Solution
@@ -64,8 +159,10 @@ def run_combat(units):
     game_done = False
     while not game_done:
         for unit in sorted(units, key=lambda unit: unit.get_position()):
-            game_done = unit.tick()
-            if game_done:
+            try:
+                unit.tick()
+            except NoMoreTargetsException:
+                game_done = True
                 break
         else:
             num_rounds += 1
