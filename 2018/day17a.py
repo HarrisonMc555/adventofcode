b9ae1a69e3a8bddb9826c62bf156ed75bb1540be
Harrison McCullough <mccullough.harrison@gmail.com>
2018-12-17 22:05:33 -0700

    Almost finish day17a

--- 2018/day17a.py
+++ 2018/day17a.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python3
-#pylint: disable=invalid-name
+#pylint: disable=invalid-name, too-few-public-methods
 
 import sys
 import re
 from enum import Enum, auto
+from collections import defaultdict
 
 ################################################################################
 # Classes
@@ -15,6 +16,15 @@ class Square(Enum):
     # WATER_FALLING = auto()
     # WATER_AT_REST = auto()
 
+    def is_sand(self):
+        return self == Square.SAND
+
+    def is_clay(self):
+        return self == Square.CLAY
+
+    def is_water(self):
+        return self == Square.WATER
+
     def to_char(self):
         if self == Square.SAND:
             return '.'
@@ -24,30 +34,102 @@ class Square(Enum):
             return '|'
         raise Exception('Invalid Square value:', self)
 
+class Grid:
+    def __init__(self, coordinates):
+        x_values = {x for x, _ in coordinates}
+        y_values = {y for _, y in coordinates}
+        min_x, max_x = min(x_values), max(x_values)
+        min_y, max_y = min(y_values), max(y_values)
+        # you can overflow from the sides of the edges, but that's as far in x
+        # as you'll ever go
+        min_x -= 1
+        max_x += 1
+        self.squares = defaultdict(lambda: defaultdict(lambda: Square.SAND))
+        for x, y in coordinates:
+            self.squares[y][x] = Square.CLAY
+        self.min_row, self.max_row = min_y, max_y
+        self.min_col, self.max_col = min_x, max_x
+
+    def __getitem__(self, indices):
+        return self.squares[indices]
+
+    def create_printable_string(self):
+        lines = []
+        for row in range(self.min_row, self.max_row + 1):
+            line = []
+            for col in range(self.min_col, self.max_col + 1):
+                line.append(self.squares[row][col].to_char())
+            lines.append(line)
+        return '\n'.join(''.join(line) for line in lines)
+
 ################################################################################
 # Run
 ################################################################################
-SPRING_X, SPRING_Y = 500, 0
-SPRING_ROW, SPRING_COL = SPRING_Y, SPRING_X
+SPRING_COL = 500
 def solve(grid):
-    print(create_grid_string(grid))
-    # return grid
+    print(grid.create_printable_string())
+    fill_with_water(grid, SPRING_COL)
+    print(grid.create_printable_string())
+    return count_water_squares(grid)
 
-def fill_with_water(grid, spring_location):
-    spring_row, spring_col = spring_location
-    min_row, max_row = min(grid), max(grid)
-    row, col = min_row, spring_col
-    while row <= max_row:
-        pass
+def count_water_squares(grid):
+    num_water_squares = 0
+    for row in grid.squares.values():
+        for square in row.values():
+            if square.is_water():
+                num_water_squares += 1
+    return num_water_squares
 
-def create_grid_string(grid):
-    lines = []
-    for _, row in sorted(grid.items()):
-        line = []
-        for _, square in sorted(row.items()):
-            line.append(square.to_char())
-        lines.append(line)
-    return '\n'.join(''.join(line) for line in lines)
+def fill_with_water(grid, spring_col):
+    row, col = grid.min_row, spring_col
+    fill_down_from(grid, (row, col))
+
+def fill_down_from(grid, location):
+    print('fill_down_from:', location)
+    row, col = location
+    if grid[row][col].is_water():
+        print('\t', 'exiting early')
+        return
+    while row <= grid.max_row and not grid[row][col].is_clay():
+        # print('\t', 'water fell down to', (row, col))
+        grid[row][col] = Square.WATER
+        row += 1
+    # print(grid.create_printable_string())
+    if row <= grid.max_row and grid[row][col].is_clay():
+        fill_across_from(grid, (row - 1, col))
+
+def fill_across_from(grid, location):
+    print('fill_across_from:', location)
+    row, start_col = location
+    # go left
+    col = start_col
+    # square must be sand or water (i.e. not clay)
+    # below must be clay or water (i.e. not sand)
+    # print('\tgoing left...')
+    # while not grid[row][col].is_clay() and not grid[row + 1][col].is_sand():
+    while not grid[row][col].is_clay() and not grid[row + 1][col].is_sand():
+        # print('\t', 'water spread across to', (row, col))
+        grid[row][col] = Square.WATER
+        col -= 1
+    if grid[row][col].is_sand() and grid[row + 1][col].is_sand():
+        # print('\t', 'falling over to the left at', (row, col))
+        fill_down_from(grid, (row, col))
+    hit_wall_left = grid[row][col].is_clay()
+    # go right
+    col = start_col + 1
+    # print('\tgoing right...')
+    while not grid[row][col].is_clay() and not grid[row + 1][col].is_sand():
+        # print('\t', 'water spread across to', (row, col))
+        grid[row][col] = Square.WATER
+        col += 1
+    if grid[row][col].is_sand() and grid[row + 1][col].is_sand():
+        # print('\t', 'falling over to the right at', (row, col))
+        fill_down_from(grid, (row, col))
+    hit_wall_right = grid[row][col].is_clay()
+    # possibly go up
+    if hit_wall_left and hit_wall_right:
+        # print('\tgoing up...')
+        fill_across_from(grid, (row - 1, start_col))
 
 ################################################################################
 # Input
@@ -56,7 +138,8 @@ def get_input():
     coordinates = set()
     for line in sys.stdin.readlines():
         coordinates.update(parse_line(line.strip()))
-    return build_grid(coordinates)
+    # return build_grid(coordinates)
+    return Grid(coordinates)
 
 PATTERN = re.compile(r'([xy])=(\d+), ([xy])=(\d+)..(\d+)')
 def parse_line(line):
@@ -69,19 +152,21 @@ def parse_line(line):
         coordinates = [(b, a) for (a, b) in coordinates]
     return coordinates
 
-def build_grid(coordinates):
-    x_values = {x for x, _ in coordinates}
-    y_values = {y for _, y in coordinates}
-    min_x, max_x = min(x_values), max(x_values)
-    min_y, max_y = min(y_values), max(y_values)
-    grid = {}
-    for y in range(min_y, max_y + 1):
-        row = {}
-        for x in range(min_x, max_x + 1):
-            is_clay = (x, y) in coordinates
-            row[x] = Square.CLAY if is_clay else Square.SAND
-        grid[y] = row
-    return grid
+# def build_grid(coordinates):
+#     x_values = {x for x, _ in coordinates}
+#     y_values = {y for _, y in coordinates}
+#     min_x, max_x = min(x_values), max(x_values)
+#     min_y, max_y = min(y_values), max(y_values)
+#     grid = {}
+#     for x, y in coordinates:
+#         grid[y][x] = Square.CLAY
+#     for y in range(min_y, max_y + 1):
+#         row = defaultdict(lambda: Square.SAND)
+#         for x in range(min_x, max_x + 1):
+#             is_clay = (x, y) in coordinates
+#             row[x] = Square.CLAY if is_clay else Square.SAND
+#         grid[y] = row
+#     return grid
 
 ################################################################################
 # Run
