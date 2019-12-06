dc9762e99913e93a6b38674ab0f4505f370c8314
Harrison McCullough <mccullough.harrison@gmail.com>
2019-12-05 23:43:42 -0700

    Finish 2019-02b

--- 2019/src/day02.rs
+++ 2019/src/day02.rs
@@ -9,16 +9,46 @@ const OPCODE_ADD: Value = 1;
 const OPCODE_MUL: Value = 2;
 const OPCODE_HALT: Value = 99;
 
+const MAX_NOUN: Value = 99;
+const MAX_VERB: Value = 99;
+
+const DEFAULT_NOUN: Value = 12;
+const DEFAULT_VERB: Value = 2;
+
+const OUTPUT_GOAL: Value = 19690720;
+
 type Op = fn(Value, Value) -> Value;
 
 pub fn main() {
-    let answer1 = solve1(INPUT).unwrap();
+    let answer1 = solve1(INPUT, DEFAULT_NOUN, DEFAULT_VERB).unwrap();
+    let answer2 = solve2(INPUT, OUTPUT_GOAL).unwrap();
     println!("{}", answer1);
+    println!("{}", answer2);
 }
 
-fn solve1(input: &str) -> Result<Value> {
-    let mut program = parse_input(input)?;
-    alter_program(&mut program)?;
+fn solve1(input: &str, noun: Value, verb: Value) -> Result<Value> {
+    let program = parse_input(input)?;
+    run_altered(program, noun, verb)
+}
+
+fn solve2(input: &str, output_goal: Value) -> Result<Value> {
+    let program = parse_input(input)?;
+    for noun in 0..=MAX_NOUN {
+        for verb in 0..=MAX_VERB {
+            if run_altered(program.clone(), noun, verb) == Ok(output_goal) {
+                return Ok(100 * noun + verb);
+            }
+        }
+    }
+    Err(())
+}
+
+fn run_altered(mut program: Vec<Value>, noun: Value, verb: Value) -> Result<Value> {
+    alter_program(&mut program, noun, verb)?;
+    run(program)
+}
+
+fn run(mut program: Vec<Value>) -> Result<Value> {
     let mut index = 0;
     while let Some(new_index) = step(&mut program, index)? {
         index = new_index;
@@ -45,9 +75,9 @@ fn step(program: &mut [Value], index: usize) -> Result<Option<usize>> {
     Ok(Some(index + 4))
 }
 
-fn alter_program(program: &mut [Value]) -> Result<()> {
-    *get_mut_or_err(program, 1)? = 12;
-    *get_mut_or_err(program, 2)? = 2;
+fn alter_program(program: &mut [Value], noun: Value, verb: Value) -> Result<()> {
+    *get_mut_or_err(program, 1)? = noun;
+    *get_mut_or_err(program, 2)? = verb;
     Ok(())
 }
 
