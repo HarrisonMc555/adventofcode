f11a86fb46ed5de38eaf7d1c74c81041bcf83ac1
Harrison McCullough <mccullough.harrison@gmail.com>
2019-12-05 23:27:43 -0700

    Update 2019-01

--- 2019/src/day01.rs
+++ 2019/src/day01.rs
@@ -1,27 +1,28 @@
 type Result<T> = std::result::Result<T, ()>;
+type Value = u32;
 
 const INPUT: &str = include_str!("../static/day01.txt");
 
 pub fn main() {
-    let answer = solve(INPUT).unwrap();
+    let answer1 = solve1(INPUT).unwrap();
     let answer2 = solve2(INPUT).unwrap();
-    println!("{}", answer);
+    println!("{}", answer1);
     println!("{}", answer2);
 }
 
-fn solve2(input: &str) -> Result<u64> {
+fn solve1(input: &str) -> Result<Value> {
+    let masses = parse(input)?;
+    let fuel = masses.into_iter().map(calc_fuel).sum();
+    Ok(fuel)
+}
+
+fn solve2(input: &str) -> Result<Value> {
     let masses = parse(input)?;
     let total_fuel = masses.into_iter().map(calc_fuel_recursive).sum();
     Ok(total_fuel)
 }
 
-fn solve(input: &str) -> Result<u64> {
-    let masses = parse(input)?;
-    let answer = masses.into_iter().map(calc_fuel).sum();
-    Ok(answer)
-}
-
-fn calc_fuel_recursive(mass: u64) -> u64 {
+fn calc_fuel_recursive(mass: Value) -> Value {
     let new_fuel = calc_fuel(mass);
     if new_fuel == 0 {
         return 0;
@@ -29,7 +30,7 @@ fn calc_fuel_recursive(mass: u64) -> u64 {
     new_fuel + calc_fuel_recursive(new_fuel)
 }
 
-fn calc_fuel(mass: u64) -> u64 {
+fn calc_fuel(mass: Value) -> Value {
     let quotient = mass / 3;
     if quotient < 2 {
         0
@@ -38,6 +39,6 @@ fn calc_fuel(mass: u64) -> u64 {
     }
 }
 
-fn parse(input: &str) -> Result<Vec<u64>> {
+fn parse(input: &str) -> Result<Vec<Value>> {
     input.lines().map(|s| s.parse().map_err(|_| ())).collect()
 }
