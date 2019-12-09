f4d40c566c8880a3d128a1f4122458b0ce7bfcb7
Harrison McCullough <mccullough.harrison@gmail.com>
2019-12-09 13:35:51 -0700

    Refactor IntCode to contain index

--- 2019/src/intcode.rs
+++ 2019/src/intcode.rs
@@ -1,7 +1,15 @@
 use std::ops;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-pub struct IntCode(Vec<Value>);
+pub struct IntCode {
+    memory: Vec<Value>,
+    index: usize,
+}
+
+enum Going {
+    Continue,
+    Stop,
+}
 
 pub type Error = ();
 pub type Result<T> = std::result::Result<T, Error>;
@@ -21,17 +29,23 @@ impl IntCode {
         s.parse()
     }
 
-    pub fn altered(mut self, noun: Value, verb: Value) -> Result<Self> {
-        self.alter(noun, verb)?;
-        Ok(self)
+    fn new(memory: Vec<Value>) -> Self {
+        IntCode { memory, index: 0 }
     }
 
     pub fn run(mut self) -> Result<Value> {
-        let mut index = 0;
-        while let Some(new_index) = self.step(index)? {
-            index = new_index;
+        loop {
+            match self.step()? {
+                Going::Continue => (),
+                Going::Stop => break,
+            }
         }
         self.get(0)
+    }
+
+    pub fn altered(mut self, noun: Value, verb: Value) -> Result<Self> {
+        self.alter(noun, verb)?;
+        Ok(self)
     }
 
     fn alter(&mut self, noun: Value, verb: Value) -> Result<()> {
@@ -40,30 +54,36 @@ impl IntCode {
         Ok(())
     }
 
-    fn step(&mut self, index: usize) -> Result<Option<usize>> {
-        let opcode = self.get(index)?;
+    fn step(&mut self) -> Result<Going> {
+        let opcode = self.next()?;
         let op: BinaryOp = match opcode {
             OPCODE_ADD => ops::Add::add,
             OPCODE_MUL => ops::Mul::mul,
-            OPCODE_HALT => return Ok(None),
+            OPCODE_HALT => return Ok(Going::Stop),
             _ => return Err(()),
         };
-        let op1_index = self.get(index + 1)?;
-        let op2_index = self.get(index + 2)?;
-        let dest_index = self.get(index + 3)?;
+        let op1_index = self.next()?;
+        let op2_index = self.next()?;
+        let dest_index = self.next()?;
         let op1 = self.get(op1_index)?;
         let op2 = self.get(op2_index)?;
         let dest = self.get_mut(dest_index)?;
         *dest = op(op1, op2);
-        Ok(Some(index + 4))
+        Ok(Going::Continue)
+    }
+
+    fn next(&mut self) -> Result<Value> {
+        let value = self.get(self.index)?;
+        self.index += 1;
+        Ok(value)
     }
 
     fn get(&self, index: usize) -> Result<Value> {
-        self.0.get(index).copied().ok_or(())
+        self.memory.get(index).copied().ok_or(())
     }
 
     fn get_mut(&mut self, index: usize) -> Result<&mut Value> {
-        self.0.get_mut(index).ok_or(())
+        self.memory.get_mut(index).ok_or(())
     }
 }
 
@@ -77,6 +97,6 @@ impl std::str::FromStr for IntCode {
             .map(str::parse)
             .collect::<std::result::Result<_, _>>()
             .map_err(|_| ())?;
-        Ok(IntCode(vec))
+        Ok(IntCode::new(vec))
     }
 }
