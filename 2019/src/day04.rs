1b57152459639c0839cb350217eafeb2c3183038
Harrison McCullough <mccullough.harrison@gmail.com>
2019-12-08 21:47:28 -0700

    Finish 2019-04b

--- 2019/src/day04.rs
+++ 2019/src/day04.rs
@@ -5,29 +5,56 @@ type Value = u32;
 
 pub fn main() {
     let answer1 = solve1(INPUT).unwrap();
+    let answer2 = solve2(INPUT).unwrap();
     println!("{}", answer1);
+    println!("{}", answer2);
 }
 
 fn solve1(input: &str) -> Result<usize> {
     let (beg, end) = parse_input(input)?;
-    let count = (beg..=end).filter(|n| meets_criteria(*n)).count();
+    let count = (beg..=end).filter(|&n| meets_criteria1(n)).count();
     Ok(count)
 }
 
-fn meets_criteria(password: Value) -> bool {
+fn solve2(input: &str) -> Result<usize> {
+    let (beg, end) = parse_input(input)?;
+    let count = (beg..=end).filter(|&n| meets_criteria2(n)).count();
+    Ok(count)
+}
+
+fn meets_criteria1(password: Value) -> bool {
     let digits = password.digits().collect::<Vec<_>>();
-    is_increasing(&digits) && has_repeat(&digits)
+    is_increasing(&digits) && has_repeat(&digits, 2)
+}
+
+fn meets_criteria2(password: Value) -> bool {
+    let digits = password.digits().collect::<Vec<_>>();
+    is_increasing(&digits) && has_repeat_exact(&digits, 2)
 }
 
 fn is_increasing(nums: &[u8]) -> bool {
     windows2(nums).all(|(d1, d2)| d1 <= d2)
 }
 
-fn has_repeat<T>(slice: &[T]) -> bool
+fn has_repeat<T>(slice: &[T], length: usize) -> bool
 where
     T: PartialEq,
 {
-    windows2(slice).any(|(v1, v2)| v1 == v2)
+    slice.windows(length).any(|window| all_same(window))
+}
+
+fn has_repeat_exact<T>(slice: &[T], length: usize) -> bool
+where
+    T: PartialEq,
+{
+    Group::new(slice).any(|(_, group_len)| group_len == length)
+}
+
+fn all_same<T>(slice: &[T]) -> bool
+where
+    T: PartialEq,
+{
+    windows2(slice).all(|(v1, v2)| v1 == v2)
 }
 
 fn windows2<T>(slice: &[T]) -> impl Iterator<Item = (&T, &T)> {
@@ -87,6 +114,34 @@ trait ToDigits {
 impl ToDigits for Value {
     fn digits(&self) -> Digits {
         Digits::new(*self)
+    }
+}
+
+struct Group<'a, T> {
+    slice: &'a [T],
+    index: usize,
+}
+
+impl<'a, T> Group<'a, T> {
+    pub fn new(slice: &'a [T]) -> Self {
+        Group { slice, index: 0 }
+    }
+}
+
+impl<'a, T> Iterator for Group<'a, T>
+where
+    T: PartialEq,
+{
+    type Item = (&'a T, usize);
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let first = &self.slice.get(self.index)?;
+        let count = self.slice[self.index..]
+            .iter()
+            .take_while(|&v| v == *first)
+            .count();
+        self.index += count;
+        Some((first, count))
     }
 }
 
