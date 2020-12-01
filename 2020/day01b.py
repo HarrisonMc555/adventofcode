295864a1e57f3a5eb77c9ff30fd84cd0ff149a70
Harrison McCullough <mccullough.harrison@gmail.com>
2020-12-01 10:11:05 -0700

    Finish 2020-01b

---
+++ 2020/day01b.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+INPUT_FILE = 'input01.txt'
+SUM = 2020
+
+def main():
+    nums = get_nums(INPUT_FILE)
+    sorted_nums = list(sorted(nums))
+    for num in sorted_nums:
+        try:
+            x, y = find_pair_that_add_to(sorted_nums, SUM - num)
+            product = x * y * num
+            print(product)
+            break
+        except:
+            pass
+    else:
+        print(f'Could not find three numbers that summed to {SUM}')
+
+def get_nums(input_file):
+    with open(input_file) as f:
+        return [int(line.strip()) for line in f.readlines()]
+
+def get_nums_from_text(text):
+    return [int(line.strip()) for line in f.readlines()]    
+
+def find_pair_that_add_to(sorted_nums, total):
+    index_low = 0
+    index_high = len(sorted_nums) - 1
+    while index_low < index_high:
+        low = sorted_nums[index_low]
+        high = sorted_nums[index_high]
+        cur_total = low + high
+        if cur_total < total:
+            index_low += 1
+        elif cur_total > total:
+            index_high -= 1
+        else:
+            return (low, high)
+    raise Exception(f'Did not find pair that add to {total}')
+
+if __name__ == '__main__':
+    main()
