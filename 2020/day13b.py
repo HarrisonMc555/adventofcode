37e09ab76a33abb4178e5458645921590de7cca5
Harrison McCullough <mccullough.harrison@gmail.com>
2020-12-15 12:35:04 -0700

    Add test cases to 2020-13

--- 2020/day13b.py
+++ 2020/day13b.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
+import unittest
+
 INPUT_FILE = 'input13.txt'
 # INPUT_FILE = 'example.txt'
 
 def main():
+    # unittest.main()
     lines = get_lines(INPUT_FILE)
     bus_infos = parse_lines(lines)
     if not all(is_prime(bus_id) for bus_id, _ in bus_infos):
@@ -17,10 +20,13 @@ def get_lines(filename):
         return [line.strip() for line in f.readlines() if line]
 
 def parse_lines(lines):
-    bus_strings = lines[1].split(',')
+    return parse_text(lines[1])
+
+def parse_text(text):
+    bus_strings = text.split(',')
     return [(int(s), offset)
-               for offset, s in enumerate(bus_strings)
-               if s != 'x']
+            for offset, s in enumerate(bus_strings)
+            if s != 'x']
 
 def find_earliest_time(bus_infos):
     timestamp = 0
@@ -44,5 +50,18 @@ def get_remainder(bus_id, minutes_after):
 def is_prime(num):
     return all(num % divisor != 0 for divisor in range(2, num))
         
+class Part2Test(unittest.TestCase):
+    def test_examples(self):
+        cases = [
+            ("17,x,13,19", 3417),
+            ("67,7,59,61", 754018),
+            ("67,x,7,59,61", 779210),
+            ("67,7,x,59,61", 1261476),
+            ("1789,37,47,1889", 1202161486),
+        ]
+        for text, expected in cases:
+            bus_infos = parse_text(text)
+            self.assertEqual(expected, find_earliest_time(bus_infos))
+
 if __name__ == '__main__':
     main()
