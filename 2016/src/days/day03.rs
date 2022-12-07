2a6e8e26a24e56af0308ba07dd0fa44f1cc5bb64
Harrison McCullough <mccullough.harrison@gmail.com>
2022-12-07 15:04:44 -0700

    Finish 2016-03b

--- 2016/src/days/day03.rs
+++ 2016/src/days/day03.rs
@@ -25,13 +25,42 @@ impl Day03 {
         triangles.into_iter().filter(Triangle::is_possible).count()
     }
 
-    fn part2(&self, _example: Example, _debug: Debug) -> usize {
-        unimplemented!()
+    fn part2(&self, example: Example, _debug: Debug) -> usize {
+        let triangles = parse_triangles2(&self.read_file(example)).unwrap();
+        triangles.into_iter().filter(Triangle::is_possible).count()
     }
 }
 
 fn parse_triangles(text: &str) -> Option<Vec<Triangle>> {
     text.trim().lines().map(Triangle::parse).collect()
+}
+
+const NUM_PER_ROW: usize = 3;
+const NUM_IN_TRIANGLE: usize = 3;
+
+fn parse_triangles2(text: &str) -> Option<Vec<Triangle>> {
+    let lines: Vec<_> = text.trim().lines().map(parse_line).collect::<Option<_>>()?;
+    let mut triangles = Vec::new();
+    for column in 0..NUM_PER_ROW {
+        for rows in lines.chunks_exact(NUM_IN_TRIANGLE) {
+            let triplet = rows
+                .iter()
+                .map(|row| row.get(column).copied())
+                .collect::<Option<Vec<_>>>()?;
+            let triangle = Triangle::from_array(triplet.try_into().ok()?);
+            triangles.push(triangle);
+        }
+    }
+    Some(triangles)
+}
+
+fn parse_line(line: &str) -> Option<[usize; NUM_PER_ROW]> {
+    line.trim()
+        .split_whitespace()
+        .map(|w| w.parse().ok())
+        .collect::<Option<Vec<_>>>()?
+        .try_into()
+        .ok()
 }
 
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash)]
@@ -52,13 +81,15 @@ impl Triangle {
             static ref RE: Regex = Regex::new(r"^\s*(\d+)\s+(\d+)\s+(\d+)\s*$").unwrap();
         }
         let caps = RE.captures(line)?;
-        let parse = |group| {
-            caps.get(group).unwrap().as_str().parse().ok()
-        };
+        let parse = |group| caps.get(group).unwrap().as_str().parse().ok();
         let a = parse(1)?;
         let b = parse(2)?;
         let c = parse(3)?;
         Some(Triangle { a, b, c })
+    }
+
+    fn from_array([a, b, c]: [usize; NUM_IN_TRIANGLE]) -> Self {
+        Triangle { a, b, c }
     }
 }
 
