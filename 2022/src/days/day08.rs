b62a72a42da871318507e3f1461042320d492d2f
Harrison McCullough <mccullough.harrison@gmail.com>
2022-12-07 22:51:52 -0700

    Finish 2022-08b

--- 2022/src/days/day08.rs
+++ 2022/src/days/day08.rs
@@ -24,23 +24,20 @@ impl Day08 {
         count_visible(&trees)
     }
 
-    fn part2(&self, _example: Example, _debug: Debug) -> usize {
-        todo!()
+    fn part2(&self, example: Example, _debug: Debug) -> usize {
+        let text = self.read_file(example);
+        let trees = parse_trees(&text).unwrap();
+        find_best_score(&trees)
     }
 }
 
 const BASE: u32 = 10;
 
 fn count_visible(trees: &Array2D<u32>) -> usize {
-    let mut count = 0;
-    for row in 0..trees.num_rows() {
-        for column in 0..trees.num_columns() {
-            if is_visible(trees, row, column) {
-                count += 1;
-            }
-        }
-    }
-    count
+    (0..trees.num_rows())
+        .flat_map(|r| (0..trees.num_columns()).map(move |c| (r, c)))
+        .filter(|(r, c)| is_visible(trees, *r, *c))
+        .count()
 }
 
 fn is_visible(trees: &Array2D<u32>, row: usize, column: usize) -> bool {
@@ -58,6 +55,43 @@ fn is_visible(trees: &Array2D<u32>, row: usize, column: usize) -> bool {
         || below.map(to_tree).all(lower)
         || left.map(to_tree).all(lower)
         || right.map(to_tree).all(lower)
+}
+
+fn find_best_score(trees: &Array2D<u32>) -> usize {
+    (0..trees.num_rows())
+        .flat_map(|r| (0..trees.num_columns()).map(move |c| (r, c)))
+        .map(|(r, c)| score(trees, r, c))
+        .max()
+        .unwrap_or(0)
+}
+
+fn score(trees: &Array2D<u32>, row: usize, column: usize) -> usize {
+    let tree = trees[(row, column)];
+    let to_tree = |(r, c)| trees[(r, c)];
+
+    let above = (0..row).map(|r| (r, column)).rev();
+    let below = ((row + 1)..trees.num_rows()).map(|r| (r, column));
+    let left = (0..column).map(|c| (row, c)).rev();
+    let right = ((column + 1)..trees.num_columns()).map(|c| (row, c));
+
+    score_direction(above.map(to_tree), tree)
+        * score_direction(below.map(to_tree), tree)
+        * score_direction(left.map(to_tree), tree)
+        * score_direction(right.map(to_tree), tree)
+}
+
+fn score_direction<T>(iter: T, tree: u32) -> usize
+where
+    T: Iterator<Item = u32>,
+{
+    let mut count = 0;
+    for t in iter {
+        count += 1;
+        if t >= tree {
+            break;
+        }
+    }
+    count
 }
 
 fn parse_trees(text: &str) -> Option<Array2D<u32>> {
@@ -86,5 +120,10 @@ mod test {
     }
 
     #[test]
-    fn test_examples_part2() {}
+    fn test_examples_part2() {
+        let text = include_str!("../../static/example08.txt");
+        let trees = parse_trees(text).unwrap();
+        let best_score = find_best_score(&trees);
+        assert_eq!(8, best_score);
+    }
 }
