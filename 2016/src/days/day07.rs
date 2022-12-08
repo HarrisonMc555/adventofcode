7f72dd6dd1275ec535fb653ce87649970b2214c6
Harrison McCullough <mccullough.harrison@gmail.com>
2022-12-08 14:15:58 -0700

    Finish 2016-07b

--- 2016/src/days/day07.rs
+++ 2016/src/days/day07.rs
@@ -1,4 +1,5 @@
 use crate::days::{Day, Debug, Example, Part};
+use std::collections::HashSet;
 
 pub struct Day07;
 
@@ -18,11 +19,17 @@ impl Day for Day07 {
 
 impl Day07 {
     fn part1(&self, example: Example, _debug: Debug) -> usize {
-        self.get_lines(example).into_iter().filter(|line| supports_tls(line)).count()
+        self.get_lines(example)
+            .into_iter()
+            .filter(|line| supports_tls(line))
+            .count()
     }
 
-    fn part2(&self, _example: Example, _debug: Debug) -> usize {
-        todo!()
+    fn part2(&self, example: Example, _debug: Debug) -> usize {
+        self.get_lines(example)
+            .into_iter()
+            .filter(|line| supports_ssl(line))
+            .count()
     }
 }
 
@@ -35,7 +42,7 @@ fn supports_tls<T: AsRef<str>>(ip_addr: T) -> bool {
         match window[0] {
             '[' => in_hypernet_sequence = true,
             ']' => in_hypernet_sequence = false,
-            _ => {},
+            _ => {}
         }
         if is_abba(window.try_into().unwrap()) {
             if in_hypernet_sequence {
@@ -51,6 +58,47 @@ fn supports_tls<T: AsRef<str>>(ip_addr: T) -> bool {
 fn is_abba(window: &[char; ABBA_LEN]) -> bool {
     let [a, b, c, d] = window;
     a == d && b == c && a != b
+}
+
+const ABA_LEN: usize = 3;
+fn supports_ssl<T: AsRef<str>>(ip_addr: T) -> bool {
+    let mut in_hypernet_sequence = false;
+    let chars = ip_addr.as_ref().chars().collect::<Vec<_>>();
+    let mut abas = HashSet::new();
+    let mut babs = HashSet::new();
+    for window in chars.windows(ABA_LEN) {
+        match window[0] {
+            '[' => in_hypernet_sequence = true,
+            ']' => in_hypernet_sequence = false,
+            _ => {}
+        }
+        let window = window.try_into().unwrap();
+        if is_aba(window) {
+            if in_hypernet_sequence {
+                if abas.contains(&create_bab(window)) {
+                    return true;
+                }
+                babs.insert(window);
+            } else {
+                if babs.contains(&create_bab(window)) {
+                    return true;
+                }
+                abas.insert(window);
+            }
+        }
+    }
+    false
+    // abas.into_iter().map(create_bab).any(|bab| babs.contains(&bab))
+}
+
+fn is_aba(window: &[char; ABA_LEN]) -> bool {
+    let [a, b, c] = window;
+    a == c && a != b
+}
+
+fn create_bab(window: &[char; ABA_LEN]) -> [char; ABA_LEN] {
+    let [a, b, _] = window;
+    [*b, *a, *b]
 }
 
 #[cfg(test)]
@@ -72,10 +120,14 @@ mod test {
 
     #[test]
     fn test_examples_part2() {
+        assert!(supports_ssl("aba[bab]xyz"));
+        assert!(!supports_ssl("xyx[xyx]xyx"));
+        assert!(supports_ssl("aaa[kek]eke"));
+        assert!(supports_ssl("zazbz[bzb]cdb"));
     }
 
     #[test]
     fn test_real_part2() {
-        // assert_eq!("999828ec", Day07.part2(Example::Real, Debug::NotDebug));
+        assert_eq!(242, Day07.part2(Example::Real, Debug::NotDebug));
     }
 }
