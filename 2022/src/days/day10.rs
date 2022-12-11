1648764da5d51fa7aec3411a61d91d7f3ab8ff61
Harrison McCullough <mccullough.harrison@gmail.com>
2022-12-10 17:35:05 -0700

    Finish 2022-10b

--- 2022/src/days/day10.rs
+++ 2022/src/days/day10.rs
@@ -1,6 +1,10 @@
 use crate::days::{Day, Debug, Example, Part};
+use crate::debug_println;
+use array2d::Array2D;
 
 pub struct Day10;
+
+const DEBUG: bool = false;
 
 impl Day for Day10 {
     fn number(&self) -> u32 {
@@ -17,14 +21,16 @@ impl Day for Day10 {
 }
 
 impl Day10 {
-    fn part1(&self, example: Example, _debug: Debug) -> i32 {
+    fn part1(&self, example: Example, _debug: Debug) -> String {
         let instructions = parse(&self.read_file(example)).unwrap();
         let register_history = run_instructions(&instructions);
-        calc_signal_strength(&register_history, &SPECIAL_CYCLES)
+        calc_signal_strength(&register_history, &SPECIAL_CYCLES).to_string()
     }
 
-    fn part2(&self, _example: Example, _debug: Debug) -> i32 {
-        todo!()
+    fn part2(&self, example: Example, _debug: Debug) -> String {
+        let instructions = parse(&self.read_file(example)).unwrap();
+        let screen = draw_screen(&instructions);
+        screen_strings(&screen).join("\n")
     }
 }
 
@@ -34,6 +40,13 @@ const SPECIAL_CYCLES: [usize; 6] = [20, 60, 100, 140, 180, 220];
 enum Instruction {
     Noop,
     Addx(i32),
+}
+
+#[derive(Debug, Hash, Default, Copy, Clone, Eq, PartialEq)]
+enum Cell {
+    #[default]
+    Off,
+    On,
 }
 
 fn run_instructions(instructions: &[Instruction]) -> Vec<i32> {
@@ -50,7 +63,77 @@ fn run_instructions(instructions: &[Instruction]) -> Vec<i32> {
 }
 
 fn calc_signal_strength(register_history: &[i32], special_cycles: &[usize]) -> i32 {
-    special_cycles.iter().map(|cycle| *cycle as i32 * register_history[*cycle - 1]).sum()
+    special_cycles
+        .iter()
+        .map(|cycle| *cycle as i32 * register_history[*cycle - 1])
+        .sum()
+}
+
+const SCREEN_WIDTH: usize = 40;
+const SCREEN_HEIGHT: usize = 6;
+fn draw_screen(instructions: &[Instruction]) -> Array2D<Cell> {
+    let register_history = run_instructions(instructions);
+    let mut screen = Array2D::filled_with(Cell::Off, SCREEN_HEIGHT, SCREEN_WIDTH);
+    for (cycle, register) in register_history.into_iter().enumerate() {
+        debug_println!(
+            "During cycle    {: >2}: CRT draws in position {}",
+            cycle + 1,
+            cycle % screen.num_columns()
+        );
+        debug_println!(
+            "Current CRT row   : {}",
+            screen
+                .row_iter(cycle / screen.num_columns())
+                .map(|row| row
+                    .take(cycle % screen.num_columns())
+                    .map(|cell| cell.to_char())
+                    .collect::<String>())
+                .unwrap_or_else(|_| "???".to_string())
+        );
+        debug_println!(
+            "Sprite position {: <2}: {}",
+            register,
+            (0..screen.num_columns())
+                .map(|i| {
+                    if register - 1 <= i as i32 && i as i32 <= register + 1 {
+                        '#'
+                    } else {
+                        '.'
+                    }
+                })
+                .collect::<String>()
+        );
+        debug_println!();
+        draw_cell(&mut screen, cycle, register);
+    }
+    screen
+}
+
+fn screen_strings(screen: &Array2D<Cell>) -> Vec<String> {
+    screen
+        .rows_iter()
+        .map(|row| row.map(|cell| cell.to_char()).collect())
+        .collect()
+}
+
+fn draw_cell(screen: &mut Array2D<Cell>, cycle: usize, register: i32) {
+    if !sprite_overlaps(screen, cycle, register) {
+        return;
+    }
+    if let Some(cell) = screen.get_mut_row_major(cycle) {
+        *cell = Cell::On;
+    }
+}
+
+fn sprite_overlaps(screen: &Array2D<Cell>, cycle: usize, register: i32) -> bool {
+    if register < 0 {
+        return false;
+    }
+    let register = register as usize;
+    let min = register.saturating_sub(1);
+    let max = register + 1;
+    let cycle_index = cycle % screen.num_columns();
+    min <= cycle_index && cycle_index <= max
 }
 
 impl Instruction {
@@ -80,6 +163,15 @@ impl Instruction {
 
         let addx_amount = text.strip_prefix("addx ")?.parse().ok()?;
         Some(Instruction::Addx(addx_amount))
+    }
+}
+
+impl Cell {
+    fn to_char(self) -> char {
+        match self {
+            Cell::Off => '.',
+            Cell::On => '#',
+        }
     }
 }
 
@@ -125,10 +217,39 @@ mod test {
 
     #[test]
     fn test_real_part1() {
-        assert_eq!(17180, Day10.part1(Example::Real, Debug::NotDebug));
+        assert_eq!("17180", Day10.part1(Example::Real, Debug::NotDebug));
     }
 
     #[test]
     fn test_examples_part2() {
+        let text = include_str!("../../static/example10.txt");
+        let instructions = parse(text).unwrap();
+        let screen = draw_screen(&instructions);
+        let actual = screen_strings(&screen).join("\n");
+        let expected = "##..##..##..##..##..##..##..##..##..##..\n\
+                        ###...###...###...###...###...###...###.\n\
+                        ####....####....####....####....####....\n\
+                        #####.....#####.....#####.....#####.....\n\
+                        ######......######......######......####\n\
+                        #######.......#######.......#######.....";
+        if expected != actual {
+            println!("Expected:\n{}\n\nActual:\n{}\n\n", expected, actual);
+        }
+        assert_eq!(expected, actual);
+    }
+
+    #[test]
+    fn test_real_part2() {
+        let actual = Day10.part2(Example::Real, Debug::NotDebug);
+        let expected = "###..####.#..#.###..###..#....#..#.###..\n\
+                        ...#.#....#..#.#..#.#..#.#....#..#.#..#.\n\
+                        ...#.###..####.#..#.#..#.#....#..#.###..\n\
+                        ###..#....#..#.###..###..#....#..#.#..#.\n\
+                        ..#..#....#..#.#....#.#..#....#..#.#..#.\n\
+                        ...#.####.#..#.#....#..#.####..##..###..";
+        if expected != actual {
+            println!("Expected:\n{}\n\nActual:\n{}\n\n", expected, actual);
+        }
+        assert_eq!(expected, actual);
     }
 }
