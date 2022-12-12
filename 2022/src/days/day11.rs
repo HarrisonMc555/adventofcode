5a110d778ff6464b62fd9d5af0f8b93f578e9ba3
Harrison McCullough <mccullough.harrison@gmail.com>
2022-12-11 18:27:08 -0700

    Improve 2022-11: Add id to item

--- 2022/src/days/day11.rs
+++ 2022/src/days/day11.rs
@@ -66,8 +66,11 @@ enum Amount {
 
 #[derive(Debug, Clone, Eq, PartialEq)]
 struct MonkeyID(usize);
-#[derive(Debug, Clone, Eq, PartialEq)]
-struct Item(u32);
+#[derive(Debug, Eq, PartialEq)]
+struct Item {
+    id: String,
+    worry_level: u32,
+}
 
 fn calc_monkey_business(monkeys: &mut [Monkey], num_rounds: usize) -> Option<usize> {
     for (index, monkey) in monkeys.iter().enumerate() {
@@ -110,10 +113,10 @@ fn run_turn(monkeys: &mut [Monkey], monkey_index: usize) {
 
 impl Monkey {
     fn inspect(&mut self) -> Option<(Item, MonkeyID)> {
-        let item = self.items.pop_front()?;
-        let result = self.test.inspect(&item);
+        let mut item = self.items.pop_front()?;
+        let dest_monkey_id = self.test.inspect(&mut item);
         self.inspection_count += 1;
-        Some(result)
+        Some((item, dest_monkey_id))
     }
 
     fn throw_item_to(&mut self, item: Item) {
@@ -122,54 +125,77 @@ impl Monkey {
 }
 
 impl Test {
-    fn inspect(&self, item: &Item) -> (Item, MonkeyID) {
-        let old = item.0;
+    fn inspect(&self, item: &mut Item) -> MonkeyID {
+        let old_worry_level = item.worry_level;
+        debug_println!(
+            "  Monkey inspects an item with a worry level of {} (item {}).",
+            old_worry_level,
+            item.id
+        );
         let operand = match self.amount {
-            Amount::Old => old,
+            Amount::Old => old_worry_level,
             Amount::Literal(literal) => literal,
         };
         let new = match self.operation {
-            Operation::Addition => old + operand,
-            Operation::Multiplication => old * operand,
+            Operation::Addition => old_worry_level + operand,
+            Operation::Multiplication => old_worry_level * operand,
         };
-        let after_boredom = new / 3;
-        let is_divisible = is_divisible_by(after_boredom, self.divisible_by);
-        let dest_monkey_id = if is_divisible {
-            &self.true_dest
-        } else {
-            &self.false_dest
-        };
-        debug_println!("  Monkey inspects an item with a worry level of {}.", old);
         debug_println!(
             "    Worry level is {} by {} to {}.",
             self.operation.verb(),
             operand,
             new,
         );
+        let final_worry_level = new / 3;
         debug_println!(
             "    Monkey gets bored with item. Worry level is divided by 3 to {}.",
-            after_boredom,
+            final_worry_level,
         );
+        let is_divisible = is_divisible_by(final_worry_level, self.divisible_by);
         debug_println!(
             "    Current worry level {} divisible by {}.",
             if is_divisible { "is" } else { "is not " },
             self.divisible_by,
         );
+        let dest_monkey_id = if is_divisible {
+            &self.true_dest
+        } else {
+            &self.false_dest
+        };
         debug_println!(
             "    Item with worry level {} is thrown to monkey {}.",
-            after_boredom,
+            final_worry_level,
             dest_monkey_id.0
         );
-        (Item(after_boredom), dest_monkey_id.clone())
+        item.change_worry_level(final_worry_level);
+        dest_monkey_id.clone()
+    }
+}
+
+impl Item {
+    fn new(id: String, worry_level: u32) -> Self {
+        Item {
+            id,
+            worry_level,
+        }
+    }
+
+    fn change_worry_level(&mut self, worry_level: u32) {
+        self.worry_level = worry_level;
     }
 }
 
 impl Monkey {
     fn parse_monkeys(text: &str) -> Option<Vec<Self>> {
-        text.split("\n\n").map(Monkey::parse).collect()
+        let mut item_id_generator = ItemIdGenerator::new();
+        let mut monkeys = Vec::new();
+        for section in text.split("\n\n") {
+            monkeys.push(Monkey::parse(section, &mut item_id_generator)?);
+        }
+        Some(monkeys)
     }
 
-    fn parse(text: &str) -> Option<Self> {
+    fn parse(text: &str, item_id_generator: &mut ItemIdGenerator) -> Option<Self> {
         lazy_static! {
             static ref RE: Regex = Regex::new(
                 "Monkey (\\d+):\\s*\
@@ -188,7 +214,7 @@ impl Monkey {
         let parse_u32 = |group| get_cap(group).parse::<u32>().ok();
         let parse_monkey_id = |group| get_cap(group).parse::<usize>().ok().map(MonkeyID);
         let id = parse_monkey_id(1)?;
-        let items = Monkey::parse_items(get_cap(2))?;
+        let items = Monkey::parse_items(get_cap(2), item_id_generator)?;
         let operation = Operation::parse(get_cap(3))?;
         let amount = Amount::parse(get_cap(4))?;
         let divisible_by = parse_u32(5)?;
@@ -210,11 +236,19 @@ impl Monkey {
         })
     }
 
-    fn parse_items(text: &str) -> Option<VecDeque<Item>> {
+    fn parse_items(text: &str, item_id_generator: &mut ItemIdGenerator) -> Option<VecDeque<Item>> {
         text.trim()
             .split(", ")
-            .map(|item| item.parse().ok().map(Item))
+            .map(|item_text| Item::parse(item_text, item_id_generator))
             .collect()
+    }
+}
+
+impl Item {
+    fn parse(text: &str, item_id_generator: &mut ItemIdGenerator) -> Option<Self> {
+        let id = item_id_generator.next();
+        let worry_level = text.parse().ok()?;
+        Some(Item::new(id, worry_level))
     }
 }
 
@@ -249,19 +283,60 @@ fn is_divisible_by(dividend: u32, divisor: u32) -> bool {
     dividend % divisor == 0
 }
 
+struct ItemIdGenerator(Vec<char>);
+
+impl ItemIdGenerator {
+    fn new() -> Self {
+        ItemIdGenerator(Vec::new())
+    }
+
+    fn next(&mut self) -> String {
+        const NUM_LETTERS: usize = 26;
+        for letter in self.0.iter_mut() {
+            if *letter == 'z' {
+                *letter = 'a';
+            } else {
+                *letter = (*letter as u8 + 1) as char;
+                return self.get_id();
+            }
+        }
+        self.0.push('a');
+        self.get_id()
+    }
+
+    fn get_id(&self) -> String {
+        self.0.iter().rev().collect()
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
 
     #[test]
+    fn test_item_id_generator() {
+        let gen = &mut ItemIdGenerator::new();
+        assert_eq!("a", gen.next());
+        assert_eq!("b", gen.next());
+        assert_eq!("c", gen.next());
+        assert_eq!("d", gen.next());
+        assert_eq!("e", gen.next());
+        while gen.next() != "z" {}
+        assert_eq!("aa", gen.next());
+        assert_eq!("ab", gen.next());
+        assert_eq!("ac", gen.next());
+    }
+
+    #[test]
     fn test_parse() {
         let text = include_str!("../../static/example11.txt");
         let monkeys = Monkey::parse_monkeys(text).unwrap();
+        let gen = &mut ItemIdGenerator::new();
 
         let actual0 = &monkeys[0];
         let expected0 = Monkey {
             id: MonkeyID(0),
-            items: create_items(&[79, 98]),
+            items: create_items(&[79, 98], gen),
             test: Test {
                 operation: Operation::Multiplication,
                 amount: Amount::Literal(19),
@@ -276,7 +351,7 @@ mod test {
         let actual1 = &monkeys[1];
         let expected1 = Monkey {
             id: MonkeyID(1),
-            items: create_items(&[54, 65, 75, 74]),
+            items: create_items(&[54, 65, 75, 74], gen),
             test: Test {
                 operation: Operation::Addition,
                 amount: Amount::Literal(6),
@@ -291,7 +366,7 @@ mod test {
         let actual2 = &monkeys[2];
         let expected2 = Monkey {
             id: MonkeyID(2),
-            items: create_items(&[79, 60, 97]),
+            items: create_items(&[79, 60, 97], gen),
             test: Test {
                 operation: Operation::Multiplication,
                 amount: Amount::Old,
@@ -306,7 +381,7 @@ mod test {
         let actual3 = &monkeys[3];
         let expected3 = Monkey {
             id: MonkeyID(3),
-            items: create_items(&[74]),
+            items: create_items(&[74], gen),
             test: Test {
                 operation: Operation::Addition,
                 amount: Amount::Literal(3),
@@ -339,7 +414,14 @@ mod test {
         // assert_eq!(0, Day11.part2(Example::Real, Debug::NotDebug));
     }
 
-    fn create_items(item_ids: &[u32]) -> VecDeque<Item> {
-        item_ids.iter().copied().map(Item).collect()
+    fn create_items(
+        worry_levels: &[u32],
+        item_id_generator: &mut ItemIdGenerator,
+    ) -> VecDeque<Item> {
+        worry_levels
+            .iter()
+            .copied()
+            .map(|worry_level| Item::new(item_id_generator.next(), worry_level))
+            .collect()
     }
 }
