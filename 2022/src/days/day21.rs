311c4cb23da25162b04e817c2a0ca265078e3fa4
Harrison McCullough <mccullough.harrison@gmail.com>
2022-12-23 09:06:58 -0700

    Finish 2022-21b

--- 2022/src/days/day21.rs
+++ 2022/src/days/day21.rs
@@ -1,10 +1,11 @@
 use std::collections::HashMap;
 use std::fmt::{Display, Formatter};
 use std::num::ParseIntError;
+use std::ops::*;
 use std::str::FromStr;
 
 use crate::days::{Day, Debug, Example, Part};
-use crate::debug_println;
+use crate::{debug_print, debug_println};
 
 const DEBUG: bool = false;
 
@@ -30,12 +31,20 @@ impl Day21 {
         evaluate(&assignments, GOAL_MONKEY_ID).unwrap()
     }
 
-    fn part2(&self, _example: Example, _debug: Debug) -> isize {
-        todo!()
+    fn part2(&self, example: Example, _debug: Debug) -> isize {
+        let assignments = parse_assignments(&self.read_file(example)).unwrap();
+        let finder = Finder::new(&assignments);
+        finder
+            .find_required_value(
+                GOAL_MONKEY_ID.parse().unwrap(),
+                CUSTOM_MONKEY_ID.parse().unwrap(),
+            )
+            .unwrap()
     }
 }
 
 const GOAL_MONKEY_ID: &str = "root";
+const CUSTOM_MONKEY_ID: &str = "humn";
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone)]
 struct Assignment {
@@ -47,6 +56,7 @@ struct Assignment {
 enum Job {
     ShoutNumber(isize),
     PerformOperation(OperationInfo),
+    CustomNumber,
 }
 
 #[derive(Debug, Hash, Eq, PartialEq, Clone)]
@@ -97,14 +107,137 @@ fn evaluate(assignments: &[Assignment], id: &str) -> Option<isize> {
                 let value1 = helper(id_to_job, cache, *id1)?;
                 let value2 = helper(id_to_job, cache, *id2)?;
                 let result = operator.apply(value1, value2);
-                debug_println!("{} result is {} ({}) {} {} ({}) = {}", id, id1, value1, operator, id2, value2, result);
+                debug_println!(
+                    "{} result is {} ({}) {} {} ({}) = {}",
+                    id,
+                    id1,
+                    value1,
+                    operator,
+                    id2,
+                    value2,
+                    result
+                );
                 cache.insert(id, result);
                 Some(result)
             }
+            Job::CustomNumber => None,
         }
     }
 
     helper(&id_to_job, &mut cache, id)
+}
+
+struct Finder<'a> {
+    id_to_job: HashMap<MonkeyID, &'a Job>,
+    cache: HashMap<MonkeyID, OperationResult>,
+}
+
+#[derive(Debug, Clone, Eq, PartialEq)]
+enum OperationResult {
+    Complete(isize),
+    Partial(Vec<(GoalAndValueToCustom, isize)>),
+}
+type GoalAndValueToCustom = fn(isize, isize) -> isize;
+
+impl<'a> Finder<'a> {
+    fn new(assignments: &'a [Assignment]) -> Self {
+        let id_to_job = assignments
+            .iter()
+            .map(|a| (a.id, &a.job))
+            // .chain(std::iter::once((custom_value_id, &Job::CustomNumber)))
+            .collect::<HashMap<_, _>>();
+        let cache = HashMap::new();
+        Finder { id_to_job, cache }
+    }
+
+    fn find_required_value(
+        mut self,
+        goal_id: MonkeyID,
+        custom_value_id: MonkeyID,
+    ) -> Option<isize> {
+        debug_println!("Finding required value for {} to make {} equal", custom_value_id, goal_id);
+        self.id_to_job.insert(custom_value_id, &Job::CustomNumber);
+        let Job::PerformOperation(OperationInfo { id1, id2, .. }) = self.id_to_job.get(&goal_id)? else {
+            debug_println!("No job found for goal: {}", goal_id);
+            return None;
+        };
+        self.evaluate_partial(*id1);
+        self.evaluate_partial(*id2);
+        let (mut goal_number, stack) =
+            match (self.cache.get(id1)?.clone(), self.cache.get(id2)?.clone()) {
+                (OperationResult::Complete(goal_number), OperationResult::Partial(f)) => {
+                    (goal_number, f)
+                }
+                (OperationResult::Partial(f), OperationResult::Complete(goal_number)) => {
+                    (goal_number, f)
+                }
+                _ => return None,
+            };
+        for (f, value) in stack.into_iter().rev() {
+            goal_number = f(goal_number, value);
+        }
+        Some(goal_number)
+    }
+
+    fn evaluate_partial(&mut self, id: MonkeyID) -> Option<()> {
+        debug_println!("Getting value for {}", id);
+        // if let Some(result) = self.cache.get(&id) {
+        if self.cache.contains_key(&id) {
+            debug_println!("\tAlready found value for {}", id);
+            return Some(());
+        }
+        let result = match self.id_to_job.remove(&id)?.clone() {
+            Job::ShoutNumber(number) => {
+                debug_println!("{} shouts {}", id, number);
+                OperationResult::Complete(number)
+            }
+            Job::PerformOperation(operation_info) => {
+                debug_print!("{} ", id);
+                self.evaluate_partial_operation(operation_info)?
+            }
+            Job::CustomNumber => {
+                debug_println!("{} is custom number", id);
+                OperationResult::Partial(Vec::new())
+            }
+        };
+        self.cache.insert(id, result);
+        Some(())
+    }
+
+    fn evaluate_partial_operation(
+        &mut self,
+        operation_info: OperationInfo,
+    ) -> Option<OperationResult> {
+        let OperationInfo { operator, id1, id2 } = operation_info;
+        self.evaluate_partial(id1)?;
+        self.evaluate_partial(id2)?;
+        let result = match (self.cache.get(&id1)?, self.cache.get(&id2)?) {
+            (OperationResult::Complete(num1), OperationResult::Complete(num2)) => {
+                OperationResult::Complete(operator.apply(*num1, *num2))
+            }
+            (OperationResult::Complete(num1), OperationResult::Partial(stack)) => {
+                let mut stack = stack.clone();
+                stack.push((operator.inverse_right_unknown(), *num1));
+                OperationResult::Partial(stack)
+            }
+            (OperationResult::Partial(stack), OperationResult::Complete(num2)) => {
+                let mut stack = stack.clone();
+                stack.push((operator.inverse_left_unknown(), *num2));
+                OperationResult::Partial(stack)
+            }
+            _ => return None,
+        };
+        // debug_println!(
+        //             "result is {} ({:?}) {} {} ({:?}) = {:?}",
+        //             id1,
+        //             value1,
+        //             operator,
+        //             id2,
+        //             value2,
+        //             result
+        //         );
+        Some(result)
+    }
 }
 
 impl Operator {
@@ -114,12 +247,44 @@ impl Operator {
 
     fn function(self) -> fn(isize, isize) -> isize {
         match self {
-            Operator::Addition => std::ops::Add::add,
-            Operator::Subtraction => std::ops::Sub::sub,
-            Operator::Multiplication => std::ops::Mul::mul,
-            Operator::Division => std::ops::Div::div,
+            Operator::Addition => Add::add,
+            Operator::Subtraction => Sub::sub,
+            Operator::Multiplication => Mul::mul,
+            Operator::Division => Div::div,
         }
     }
+
+    fn inverse_left_unknown(self) -> GoalAndValueToCustom {
+        match self {
+            Operator::Addition => Sub::sub,
+            Operator::Subtraction => Add::add,
+            Operator::Multiplication => Div::div,
+            Operator::Division => Mul::mul,
+        }
+    }
+
+    fn inverse_right_unknown(self) -> GoalAndValueToCustom {
+        match self {
+            Operator::Addition => Sub::sub,
+            Operator::Subtraction => sub_flip,
+            Operator::Multiplication => Div::div,
+            Operator::Division => div_flip,
+        }
+    }
+}
+
+fn sub_flip<T>(lhs: T, rhs: T) -> T::Output
+where
+    T: Sub<T>,
+{
+    rhs - lhs
+}
+
+fn div_flip<T>(lhs: T, rhs: T) -> T::Output
+where
+    T: Div<T>,
+{
+    rhs / lhs
 }
 
 fn parse_assignments(s: &str) -> Result<Vec<Assignment>, ParseError> {
@@ -286,12 +451,12 @@ mod test {
 
     #[test]
     fn test_examples_part2() {
-        // assert_eq!(0, Day21.part2(Example::Example, Debug::NotDebug));
+        assert_eq!(301, Day21.part2(Example::Example, Debug::NotDebug));
     }
 
     #[test]
     fn test_real_part2() {
-        // assert_eq!(0, Day21.part2(Example::Real, Debug::NotDebug));
+        assert_eq!(3360561285172, Day21.part2(Example::Real, Debug::NotDebug));
     }
 
     fn id(s: &str) -> MonkeyID {
