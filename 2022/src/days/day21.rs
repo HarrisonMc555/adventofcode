3e84cb0ae9782c7deb2eaed510627527fd515bd1
Harrison McCullough <mccullough.harrison@gmail.com>
2022-12-23 10:33:00 -0700

    Simplify 2022-21

--- 2022/src/days/day21.rs
+++ 2022/src/days/day21.rs
@@ -33,13 +33,7 @@ impl Day21 {
 
     fn part2(&self, example: Example, _debug: Debug) -> isize {
         let assignments = parse_assignments(&self.read_file(example)).unwrap();
-        let finder = Finder::new(&assignments);
-        finder
-            .find_required_value(
-                GOAL_MONKEY_ID.parse().unwrap(),
-                CUSTOM_MONKEY_ID.parse().unwrap(),
-            )
-            .unwrap()
+        find_required_value(&assignments, GOAL_MONKEY_ID, CUSTOM_MONKEY_ID).unwrap()
     }
 }
 
@@ -84,28 +78,18 @@ fn evaluate(assignments: &[Assignment], id: &str) -> Option<isize> {
         .iter()
         .map(|a| (a.id, &a.job))
         .collect::<HashMap<_, _>>();
-    let mut cache = HashMap::new();
 
-    fn helper(
-        id_to_job: &HashMap<MonkeyID, &Job>,
-        cache: &mut HashMap<MonkeyID, isize>,
-        id: MonkeyID,
-    ) -> Option<isize> {
+    fn helper(id_to_job: &HashMap<MonkeyID, &Job>, id: MonkeyID) -> Option<isize> {
         debug_println!("Getting value for {}", id);
-        if let Some(result) = cache.get(&id) {
-            debug_println!("\tAlready found value for {}: {}", id, result);
-            return Some(*result);
-        }
         match id_to_job.get(&id)? {
             Job::ShoutNumber(number) => {
                 debug_println!("{} shouts {}", id, number);
-                cache.insert(id, *number);
                 Some(*number)
             }
             Job::PerformOperation(operation_info) => {
                 let OperationInfo { operator, id1, id2 } = operation_info;
-                let value1 = helper(id_to_job, cache, *id1)?;
-                let value2 = helper(id_to_job, cache, *id2)?;
+                let value1 = helper(id_to_job, *id1)?;
+                let value2 = helper(id_to_job, *id2)?;
                 let result = operator.apply(value1, value2);
                 debug_println!(
                     "{} result is {} ({}) {} {} ({}) = {}",
@@ -117,19 +101,13 @@ fn evaluate(assignments: &[Assignment], id: &str) -> Option<isize> {
                     value2,
                     result
                 );
-                cache.insert(id, result);
                 Some(result)
             }
             Job::CustomNumber => None,
         }
     }
 
-    helper(&id_to_job, &mut cache, id)
-}
-
-struct Finder<'a> {
-    id_to_job: HashMap<MonkeyID, &'a Job>,
-    cache: HashMap<MonkeyID, OperationResult>,
+    helper(&id_to_job, id)
 }
 
 #[derive(Debug, Clone, Eq, PartialEq)]
@@ -139,105 +117,96 @@ enum OperationResult {
 }
 type GoalAndValueToCustom = fn(isize, isize) -> isize;
 
-impl<'a> Finder<'a> {
-    fn new(assignments: &'a [Assignment]) -> Self {
-        let id_to_job = assignments
-            .iter()
-            .map(|a| (a.id, &a.job))
-            // .chain(std::iter::once((custom_value_id, &Job::CustomNumber)))
-            .collect::<HashMap<_, _>>();
-        let cache = HashMap::new();
-        Finder { id_to_job, cache }
+fn find_required_value(
+    assignments: &[Assignment],
+    goal_id: &str,
+    custom_value_id: &str,
+) -> Option<isize> {
+    debug_println!(
+        "Finding required value for {} to make {} equal",
+        custom_value_id,
+        goal_id
+    );
+    let goal_id = goal_id.parse().ok()?;
+    let custom_value_id = custom_value_id.parse().ok()?;
+    let mut id_to_job = assignments
+        .iter()
+        .map(|a| (a.id, &a.job))
+        .collect::<HashMap<_, _>>();
+    id_to_job.insert(custom_value_id, &Job::CustomNumber);
+    let Job::PerformOperation(OperationInfo { id1, id2, .. }) = id_to_job.get(&goal_id)? else {
+        debug_println!("No job found for goal: {}", goal_id);
+        return None;
+    };
+    let value1 = evaluate_partial(&id_to_job, *id1)?;
+    let value2 = evaluate_partial(&id_to_job, *id2)?;
+    let (mut goal_number, stack) = match (value1, value2) {
+        (OperationResult::Complete(goal_number), OperationResult::Partial(f)) => (goal_number, f),
+        (OperationResult::Partial(f), OperationResult::Complete(goal_number)) => (goal_number, f),
+        _ => return None,
+    };
+    debug_println!("Final goal is {}", goal_number);
+    for (f, value) in stack.into_iter().rev() {
+        debug_println!("Applying inverse operation to goal {} and value {} gives us ", goal_number, value);
+        goal_number = f(goal_number, value);
+        debug_println!("{}", goal_number);
     }
+    debug_println!("Final custom value is {}", goal_number);
+    Some(goal_number)
+}
 
-    fn find_required_value(
-        mut self,
-        goal_id: MonkeyID,
-        custom_value_id: MonkeyID,
-    ) -> Option<isize> {
-        debug_println!("Finding required value for {} to make {} equal", custom_value_id, goal_id);
-        self.id_to_job.insert(custom_value_id, &Job::CustomNumber);
-        let Job::PerformOperation(OperationInfo { id1, id2, .. }) = self.id_to_job.get(&goal_id)? else {
-            debug_println!("No job found for goal: {}", goal_id);
-            return None;
-        };
-        self.evaluate_partial(*id1);
-        self.evaluate_partial(*id2);
-        let (mut goal_number, stack) =
-            match (self.cache.get(id1)?.clone(), self.cache.get(id2)?.clone()) {
-                (OperationResult::Complete(goal_number), OperationResult::Partial(f)) => {
-                    (goal_number, f)
-                }
-                (OperationResult::Partial(f), OperationResult::Complete(goal_number)) => {
-                    (goal_number, f)
-                }
-                _ => return None,
-            };
-        for (f, value) in stack.into_iter().rev() {
-            goal_number = f(goal_number, value);
+fn evaluate_partial(id_to_job: &HashMap<MonkeyID, &Job>, id: MonkeyID) -> Option<OperationResult> {
+    debug_println!("Getting value for {}", id);
+    let result = match id_to_job.get(&id)? {
+        Job::ShoutNumber(number) => {
+            debug_println!("{} shouts {}", id, number);
+            OperationResult::Complete(*number)
         }
-        Some(goal_number)
-    }
-
-    fn evaluate_partial(&mut self, id: MonkeyID) -> Option<()> {
-        debug_println!("Getting value for {}", id);
-        // if let Some(result) = self.cache.get(&id) {
-        if self.cache.contains_key(&id) {
-            debug_println!("\tAlready found value for {}", id);
-            return Some(());
+        Job::PerformOperation(operation_info) => {
+            debug_print!("{} ", id);
+            evaluate_partial_operation(id_to_job, operation_info.clone())?
         }
-        let result = match self.id_to_job.remove(&id)?.clone() {
-            Job::ShoutNumber(number) => {
-                debug_println!("{} shouts {}", id, number);
-                OperationResult::Complete(number)
-            }
-            Job::PerformOperation(operation_info) => {
-                debug_print!("{} ", id);
-                self.evaluate_partial_operation(operation_info)?
-            }
-            Job::CustomNumber => {
-                debug_println!("{} is custom number", id);
-                OperationResult::Partial(Vec::new())
-            }
-        };
-        self.cache.insert(id, result);
-        Some(())
-    }
+        Job::CustomNumber => {
+            debug_println!("{} is custom number", id);
+            OperationResult::Partial(Vec::new())
+        }
+    };
+    Some(result)
+}
 
-    fn evaluate_partial_operation(
-        &mut self,
-        operation_info: OperationInfo,
-    ) -> Option<OperationResult> {
-        let OperationInfo { operator, id1, id2 } = operation_info;
-        self.evaluate_partial(id1)?;
-        self.evaluate_partial(id2)?;
-        let result = match (self.cache.get(&id1)?, self.cache.get(&id2)?) {
-            (OperationResult::Complete(num1), OperationResult::Complete(num2)) => {
-                OperationResult::Complete(operator.apply(*num1, *num2))
-            }
-            (OperationResult::Complete(num1), OperationResult::Partial(stack)) => {
-                let mut stack = stack.clone();
-                stack.push((operator.inverse_right_unknown(), *num1));
-                OperationResult::Partial(stack)
-            }
-            (OperationResult::Partial(stack), OperationResult::Complete(num2)) => {
-                let mut stack = stack.clone();
-                stack.push((operator.inverse_left_unknown(), *num2));
-                OperationResult::Partial(stack)
-            }
-            _ => return None,
-        };
-        // debug_println!(
-        //             "result is {} ({:?}) {} {} ({:?}) = {:?}",
-        //             id1,
-        //             value1,
-        //             operator,
-        //             id2,
-        //             value2,
-        //             result
-        //         );
-        Some(result)
-    }
+fn evaluate_partial_operation(
+    id_to_job: &HashMap<MonkeyID, &Job>,
+    operation_info: OperationInfo,
+) -> Option<OperationResult> {
+    let OperationInfo { operator, id1, id2 } = operation_info;
+    let value1 = evaluate_partial(id_to_job, id1)?;
+    let value2 = evaluate_partial(id_to_job, id2)?;
+    debug_print!(
+        "result is {} ({:?}) {} {} ({:?}) = ",
+        id1,
+        value1,
+        operator,
+        id2,
+        value2
+    );
+    let result = match (value1, value2) {
+        (OperationResult::Complete(num1), OperationResult::Complete(num2)) => {
+            OperationResult::Complete(operator.apply(num1, num2))
+        }
+        (OperationResult::Complete(num1), OperationResult::Partial(stack)) => {
+            let mut stack = stack.clone();
+            stack.push((operator.inverse_right_unknown(), num1));
+            OperationResult::Partial(stack)
+        }
+        (OperationResult::Partial(stack), OperationResult::Complete(num2)) => {
+            let mut stack = stack.clone();
+            stack.push((operator.inverse_left_unknown(), num2));
+            OperationResult::Partial(stack)
+        }
+        _ => return None,
+    };
+    debug_println!("{:?}", result);
+    Some(result)
 }
 
 impl Operator {
