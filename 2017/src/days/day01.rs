4e0ec1dc40c049c90e2c3770ab8d118bad00d304
Harrison McCullough <mccullough.harrison@gmail.com>
2023-08-01 22:37:39 -0600

    Finish 2017-01

--- 2017/src/days/day01.rs
+++ 2017/src/days/day01.rs
@@ -1,6 +1,8 @@
 use crate::days::{Day, Debug, Example, Part};
-
+use itertools::Itertools;
 pub struct Day01;
+
+const RADIX: u32 = 10;
 
 impl Day for Day01 {
     fn number(&self) -> u32 {
@@ -17,13 +19,44 @@ impl Day for Day01 {
 }
 
 impl Day01 {
-    fn part1(&self, _example: Example, _debug: Debug) -> usize {
-        todo!()
+    fn part1(&self, example: Example, _debug: Debug) -> u32 {
+        let text = self.read_file(example);
+        let digits = to_digits(&text).unwrap();
+        circular_sum(&digits)
     }
 
-    fn part2(&self, _example: Example, _debug: Debug) -> usize {
-        todo!()
+    fn part2(&self, example: Example, _debug: Debug) -> u32 {
+        let text = self.read_file(example);
+        let digits = to_digits(&text).unwrap();
+        circular_across_sum(&digits)
     }
+}
+
+fn circular_sum(digits: &[u32]) -> u32 {
+    digits
+        .iter()
+        .circular_tuple_windows()
+        .filter(|(x, y)| x == y)
+        .map(|(x, _)| x)
+        .sum()
+}
+
+fn circular_across_sum(digits: &[u32]) -> u32 {
+    let offset = digits.len() / 2;
+    digits[..offset]
+        .iter()
+        .enumerate()
+        .filter(|(index, digit)| **digit == digits[index + offset])
+        .map(|(_, digit)| digit * 2)
+        .sum()
+}
+
+fn to_digits(string: &str) -> Result<Vec<u32>, char> {
+    string
+        .trim()
+        .chars()
+        .map(|c| c.to_digit(RADIX).ok_or(c))
+        .collect()
 }
 
 #[cfg(test)]
@@ -31,18 +64,37 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_examples_part1() {}
-
-    #[test]
-    fn test_real_part1() {
-        // assert_eq!(0, Day01.part1(Example::Real, Debug::NotDebug));
+    fn test_examples_part1() {
+        assert_eq!(3, circular_sum("1122"));
+        assert_eq!(4, circular_sum("1111"));
+        assert_eq!(0, circular_sum("1234"));
+        assert_eq!(9, circular_sum("91212129"));
     }
 
     #[test]
-    fn test_examples_part2() {}
+    fn test_real_part1() {
+        assert_eq!(995, Day01.part1(Example::Real, Debug::NotDebug));
+    }
+
+    #[test]
+    fn test_examples_part2() {
+        assert_eq!(6, circular_across_sum("1212"));
+        assert_eq!(0, circular_across_sum("1221"));
+        assert_eq!(4, circular_across_sum("123425"));
+        assert_eq!(12, circular_across_sum("123123"));
+        assert_eq!(4, circular_across_sum("12131415"));
+    }
 
     #[test]
     fn test_real_part2() {
-        // assert_eq!(0, Day01.part2(Example::Real, Debug::NotDebug));
+        assert_eq!(1130, Day01.part2(Example::Real, Debug::NotDebug));
+    }
+
+    fn circular_sum(string: &str) -> u32 {
+        super::circular_sum(&to_digits(string).unwrap())
+    }
+
+    fn circular_across_sum(string: &str) -> u32 {
+        super::circular_across_sum(&to_digits(string).unwrap())
     }
 }
